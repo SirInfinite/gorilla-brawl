24b17dff057da91d300ba50ab4ff06126aaa32cd
SirInfinite <99227535+SirInfinite@users.noreply.github.com>
2025-01-07 01:47:51 -0600

    added ability icons

--- gorillabrawl/Scripts/ui_menu.gd
+++ gorillabrawl/Scripts/ui_menu.gd
@@ -1,19 +1,23 @@
 extends Node2D
 
 var show_ui = false
+@onready var parent = $VBoxContainer
 
 func _ready():
+	set_process_input(true)
 	for label in get_tree().get_nodes_in_group("debug_labels"):
 		label.visible = false
+	while parent:
+		parent = parent.get_parent()
 
 func _input(event):
 	if event is InputEventKey && Input.is_action_just_pressed("debug"):
-		show_ui = !show_ui
+		show_ui = not show_ui
 		toggle_debug_label(show_ui)
 
-func toggle_debug_label(visiblity: bool):
+func toggle_debug_label(visibility: bool):
 	for label in get_tree().get_nodes_in_group("debug_labels"):
-		label.visible = visiblity
+		label.visible = visibility
 
 func _on_player_debug_info(player_pos, mouse_pos, state, velocity, direction, fps):
 	$VBoxContainer/CoordinatesLabel.text = "Position: %.2v" % player_pos
