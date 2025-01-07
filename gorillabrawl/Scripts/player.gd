0540ae69de0f25b66ba82be0cbdf2ce90003533d
SirInfinite <99227535+SirInfinite@users.noreply.github.com>
2025-01-07 17:34:08 -0600

    refactored movement

--- gorillabrawl/Scripts/player.gd
+++ gorillabrawl/Scripts/player.gd
@@ -4,11 +4,15 @@ signal debug_info
 
 @export_category("States")
 enum PlayerState { IDLE, WALKING, SPRINTING, JUMPING, FALLING, CROUCHING, SLIDING }
-@export var current_state = PlayerState.IDLE
+@export var current_state : PlayerState
 
-@export_category("Camera")
+@export_category("References")
 @onready var head = $Head
 @onready var cam = $Head/Camera3D
+@onready var hitbox = $CollisionShape3D
+@onready var body = $MeshInstance3D
+
+@export_category("Camera")
 @export var sensitivity := 1.0
 @export var fov := 75.0
 @export var fov_change := 1.5
@@ -17,13 +21,16 @@ var mouse_movement := Vector2.ZERO
 @export_category("Walking")
 @export var speed := 0.0
 @export var friction := 10.0
-const walk_speed = 5.0
-const sprint_speed = 8.0
-const crouch_speed = 3.0
+const SPEEDS = {
+	"walk": 5.0,
+	"sprint": 8.0,
+	"crouch": 3.0,
+	"slide": 12.0
+}
 
 @export_category("Jumping")
+const gravity := 9.8 # m/s^2
 @export var jump_velocity := 4.5
-@export var gravity := 9.8 # m/s^2
 @export var air_control := 3.0
 
 @export_category("Miscellaneous")
@@ -36,16 +43,15 @@ func _process(_delta : float):
 	fps = Engine.get_frames_per_second()
 
 func _physics_process(delta: float):
-	speed = handle_speed()
-
 	handle_movement(delta)
 	handle_jumping(delta)
 	handle_state()
+	handle_state_behavior(current_state)
 	handle_fov(delta)
 	
 	move_and_slide()
 	
-	debug_info.emit(global_transform.origin, mouse_movement, state_to_string(current_state), velocity, get_direction(), fps)
+	emit_debug()
 	mouse_movement = Vector2.ZERO
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -54,19 +60,29 @@ func _unhandled_input(event: InputEvent) -> void:
 			handle_mouselook(event)
 			mouse_movement = event.relative
 
-func state_to_string(state: PlayerState):
+func is_state(state: PlayerState) -> bool:
+	return current_state == state
+
+func handle_state_behavior(state: PlayerState):
 	match state:
 		PlayerState.IDLE:
+			speed = 0.0
+			handle_height(false)
 			return "IDLE"
 		PlayerState.WALKING:
+			speed = SPEEDS["walk"]
+			handle_height(false)
 			return "WALKING"
 		PlayerState.SPRINTING:
+			speed = SPEEDS["sprint"]
 			return "SPRINTING"
 		PlayerState.JUMPING:
 			return "JUMPING"
 		PlayerState.FALLING:
 			return "FALLING"
 		PlayerState.CROUCHING:
+			speed = SPEEDS["crouch"]
+			handle_height(true)
 			return "CROUCHING"
 		PlayerState.SLIDING:
 			return "SLIDING"
@@ -102,9 +118,6 @@ func get_direction() -> String:
 
 	return direction
 
-func handle_speed():
-	return sprint_speed if state_to_string(current_state) == "SPRINTING" else walk_speed
-
 func handle_mouselook(event: InputEvent):
 	head.rotate_y(-event.relative.x * (sensitivity / 1000))
 	cam.rotate_x(-event.relative.y * (sensitivity / 1000))
@@ -138,20 +151,36 @@ func handle_jumping(delta: float):
 
 func handle_state():
 	if is_on_floor():
-		if velocity.length() > 0.25:
-			if Input.is_action_pressed("sprint"):
-				current_state = PlayerState.SPRINTING
-			else:
-				current_state = PlayerState.WALKING
+		if Input.is_action_pressed("crouch"):
+			current_state = PlayerState.SLIDING if Input.is_action_pressed("sprint") else PlayerState.CROUCHING
+		elif velocity.length() > 0.25:
+			current_state = PlayerState.SPRINTING if Input.is_action_pressed("sprint") else PlayerState.WALKING
 		else:
 			current_state = PlayerState.IDLE
 	else:
-		if velocity.y > 0:
-			current_state = PlayerState.JUMPING
-		else:
-			current_state = PlayerState.FALLING
+		current_state = PlayerState.JUMPING if velocity.y > 0 else PlayerState.FALLING
 
 func handle_fov(delta):
-	var velocity_clamped = clamp(velocity.length(), 0.5, sprint_speed * 2)
-	var target_fov = fov + (fov_change * velocity_clamped * (1 if state_to_string(current_state) == "SPRINTING" else 0))
+	var velocity_clamped = clamp(velocity.length(), 0.5, SPEEDS["sprint"] * 2)
+	var target_fov = fov + (fov_change * velocity_clamped * (1 if handle_state_behavior(current_state) == "SPRINTING" else 0))
 	cam.fov = lerp(cam.fov, target_fov, 8.0 * delta)
+
+func handle_height(crouching: bool):
+	var capsule_shape = hitbox.shape as CapsuleShape3D
+	if crouching:
+		capsule_shape.height = 0.5
+		body.scale = Vector3(1, 0.5, 1)
+	else: 
+		capsule_shape.height = 1.0
+		body.scale = Vector3(1, 1, 1)
+	hitbox.shape = capsule_shape
+
+func emit_debug():
+	debug_info.emit(
+		global_transform.origin, 
+		mouse_movement, 
+		handle_state_behavior(current_state), 
+		velocity, 
+		get_direction(), 
+		fps
+		)
