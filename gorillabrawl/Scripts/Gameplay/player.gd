ca1ca473ef16e4aaae3101cddf8f28c3d67294cd
SirInfinite <99227535+SirInfinite@users.noreply.github.com>
2025-01-11 02:44:27 -0600

    refactored player code AGAIN
    omggggggggggg whyyyyyyyyyyyyyyyy

--- gorillabrawl/Scripts/Gameplay/player.gd
+++ gorillabrawl/Scripts/Gameplay/player.gd
@@ -9,50 +9,60 @@ signal debug_info
 @onready var body := $MeshInstance3D
 @onready var roofRaycast = $RoofRaycast
 
-@export_category("States")
-@export var isStanding := false
-@export var isWalking := false
-@export var isSprinting := false
-@export var isJumping := false
-@export var isCrouching := false
-@export var isSliding := false
+# States
+var states = {
+	"isStanding": false,
+	"isWalking": false,
+	"isSprinting": false,
+	"isJumping": false,
+	"isCrouching": false,
+	"isSliding": false
+}
 
-@export_category("Camera")
-@export var sensitivity := 1.0
-@export var fov := 75.0
-@export var fovChange := 1.5
-var lowerCameraClamp = -90
-var higherCameraClamp = 90
-var mouseMovement := Vector2.ZERO
-
-@export_category("Walking")
-@export var speed : float
+@export_category("Locomotion")
+@export var walk_speed = 5.0
 @export var acceleration := 15.0
 @export var deceleration := 8.5
-const SPEEDS := {
-	"walk": 5.0,
-	"sprint": 8.0,
-	"crouch": 3.0,
-	"slide": 12.0
-}
+var speed : float
+
+@export_category("Sprinting")
+@export var do_sprint = true
+@export var sprintSpeed = 8.0
+
+@export_category("Crouching")
+@export var do_crouch = true
+@export var crouch_speed = 3.0
+var crouch_release_timer := 0.0
+
+@export_category("Sliding")
+@export var do_slide = true
+@export var slide_speed = 12.0
 
 @export_category("Jumping")
 const gravity := 9.8 # m/s^2
-@export var gravityMult := 1.0
-@export var jumpVelocity := 4.5
-@export var airControl := 2.0
-@export var coyoteTimeDur := 0.1
-var coyoteTime := 0.0
-var wasOnFloor := false
+@export var gravity_mult := 1.0
+@export var jump_velocity := 4.5
+@export var air_control := 2.0
+@export var coyote_time_duration := 0.1
+var coyote_time := 0.0
 
-@export_category("Miscellaneous")
-var crouchReleaseTimer := 0.0
+@export_category("Camera")
+@export var sensitivity := 0.003
+var min_pitch = -60
+var max_pitch = 85
+var mouse_movement := Vector2.ZERO
+
+@export_category("FOV Effects")
+@export var do_fov_effects = true
+@export var fov := 80.0
+@export var fov_change := 1.25
+
+# Miscellaneous
 var fps : float
-var thirdPerson
+var third_person
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	get_tree().paused = false
 	velocity = Vector3.ZERO
 
 func _process(_delta : float):
@@ -61,89 +71,56 @@ func _process(_delta : float):
 func _physics_process(delta: float):
 	handle_movement(delta)
 	handle_jumping(delta)
+	
 	handle_state(delta)
-	handle_state_behavior()
-	handle_fov(delta)
+	handle_state_behavior(delta)
+	
+	if do_fov_effects:
+		handle_fov(delta)
 	
 	move_and_slide()
 	
 	emit_debug()
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
-		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 			handle_mouselook(event)
-			mouseMovement = event.relative
+			mouse_movement = event.relative
 
-func apply_over_time(starting_value, end_value, increment_value, mode: String = "add"):
-	if (mode == "add"):
-		if (starting_value < end_value):
-			starting_value += increment_value
-		else:
-			starting_value = end_value
-	elif (mode == "subtract"):
-		if (starting_value > end_value):
-			starting_value -= increment_value
-		else:
-			starting_value = end_value
-	elif (mode == "multiply"):
-		if (starting_value < end_value):
-			starting_value *= increment_value
-		else:
-			starting_value = end_value
-	elif (mode == "divide"):
-		if (starting_value > end_value):
-			starting_value /= increment_value
-		else:
-			starting_value = end_value
+func handle_state(delta: float):
+	states["isSprinting"] = do_sprint && Input.is_action_pressed("sprint")
 	
-	return starting_value
+	if do_crouch:
+		if Input.is_action_pressed("crouch"):
+			states["isCrouching"] = true
+		else:
+			if not roofRaycast.is_colliding():
+				crouch_release_timer += delta
+				if crouch_release_timer >= 0.1:
+					states["isCrouching"] = false
+			else:
+				crouch_release_timer = 0.0
+	
+	states["isWalking"] = is_on_floor() and Input.get_vector("left", "right", "up", "down").length() > 0.1
+	
+	states["isStanding"] = not states["isWalking"] and is_on_floor()
+	
+	states["isSliding"] = do_slide && is_on_floor() && (states["isSprinting"] and states["isCrouching"])
+	
+	states["isJumping"] = not is_on_floor()
 
-func handle_state(delta):
-	isSprinting = Input.is_action_pressed("sprint")
-	
-	if Input.is_action_pressed("crouch"):
-		isCrouching = true
-	else:
-		if not roofRaycast.is_colliding():
-			crouchReleaseTimer += delta
-			if crouchReleaseTimer >= 0.1:
-				isCrouching = false
-		else:
-			crouchReleaseTimer = 0.0
-	
-	isWalking = is_on_floor() and Input.get_vector("left", "right", "up", "down").length() > 0.1
-	
-	isStanding = not isWalking and is_on_floor()
-	
-	isSliding = isSprinting and isCrouching
-	
-	isJumping = not is_on_floor()
-	
-	if is_on_floor():
-		if Input.get_vector("left", "right", "up", "down").length() > 0.1:
-			isWalking = true
-			isStanding = false
-		else:
-			isStanding = true
-			isWalking = false
-	else:
-		isJumping = true
-
-func handle_state_behavior():
-	if isStanding:
-		handle_height(false)
-	if isWalking:
-		speed = SPEEDS["walk"]
-		handle_height(false)
-	if isSprinting:
-		speed = SPEEDS["sprint"]
-	if isCrouching:
-		speed = SPEEDS["crouch"]
-		handle_height(true)
-	if isSliding:
-		speed = SPEEDS["slide"]
-		handle_height(true)
+func handle_state_behavior(delta: float):
+	handle_height(delta)
+	if ["isStanding"]:
+		pass
+	if states["isWalking"]:
+		speed = walk_speed
+	if states["isSprinting"]:
+		speed = sprintSpeed
+	if states["isCrouching"]:
+		speed = crouch_speed
+	if states["isSliding"]:
+		speed = slide_speed
 
 func handle_direction() -> String:
 	var forward = head.global_transform.basis.z.normalized() * -1 
@@ -176,11 +153,11 @@ func handle_direction() -> String:
 		return ""
 
 func handle_mouselook(event: InputEvent):
-	head.rotate_y(-event.relative.x * (sensitivity / 1000))
-	cam.rotate_x(-event.relative.y * (sensitivity / 1000))
-	cam.rotation.x = clamp(cam.rotation.x, deg_to_rad(lowerCameraClamp), deg_to_rad(higherCameraClamp))
+	head.rotate_y(-event.relative.x * sensitivity)
+	cam.rotate_x(-event.relative.y * sensitivity)
+	cam.rotation.x = clamp(cam.rotation.x, deg_to_rad(min_pitch), deg_to_rad(max_pitch))
 	
-	mouseMovement = Vector2.ZERO
+	mouse_movement = Vector2.ZERO
 
 func handle_movement(delta: float):
 	var input = Input.get_vector("left", "right", "up", "down")
@@ -195,45 +172,39 @@ func handle_movement(delta: float):
 			velocity.x = lerp(velocity.x, 0.0, deceleration * delta)
 			velocity.z = lerp(velocity.z, 0.0, deceleration * delta)
 	else:
-		velocity.x = lerp(velocity.x, direction.x * speed, airControl * delta)
-		velocity.z = lerp(velocity.z, direction.z * speed, airControl * delta)
+		velocity.x = lerp(velocity.x, direction.x * speed, air_control * delta)
+		velocity.z = lerp(velocity.z, direction.z * speed, air_control * delta)
 
 func handle_jumping(delta: float):
 	if is_on_floor():
-		coyoteTime = coyoteTimeDur
+		coyote_time = coyote_time_duration
 	else:
-		velocity.y -= gravity * gravityMult * delta
+		velocity.y -= gravity * gravity_mult * delta
 		
-		if coyoteTime > 0:
-			coyoteTime -= delta
+		if coyote_time > 0:
+			coyote_time -= delta
 	
-	if Input.is_action_just_pressed("jump"):
-		if is_on_floor() or coyoteTime > 0:
-			velocity.y = jumpVelocity
-			coyoteTime = 0
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or coyote_time > 0):
+		velocity.y = jump_velocity
+		coyote_time = 0
 
 func handle_fov(delta):
-	var targetFOV = fov + (fovChange * (1 if isSprinting else 0))
+	var targetFOV = fov + (fov_change if states["isSprinting"] else 0)
 	cam.fov = lerp(cam.fov, targetFOV, 8.0 * delta)
 	
-	var target_rotation = deg_to_rad(900) if isSliding else cam.rotation_degrees.x
+	var target_rotation = deg_to_rad(-10) if states["isSliding"] else cam.rotation_degrees.x
 	cam.rotation_degrees.x = lerp(cam.rotation_degrees.x, target_rotation, 8.0 * delta)
 
-func handle_height(crouching: bool):
-	var capsule_shape = hitbox.shape as CapsuleShape3D
+func handle_height(delta):
+	var capsule_shape = hitbox.shape
 	
-	if crouching:
-		# capsule_shape.height = apply_over_time(capsule_shape.height, 0.5, 0.15, "subtract")
-		capsule_shape.height = 0.5
-	else: 
-		# capsule_shape.height = apply_over_time(capsule_shape.height, 1, 0.15, "add")
-		capsule_shape.height = 1
-	hitbox.shape = capsule_shape
+	capsule_shape.height = lerp(capsule_shape.height, 0.5 if states["isCrouching"] else (0.4 if states["isSliding"] else 1.0), 10.0 * delta)
 
 func emit_debug():
 	debug_info.emit(
 		global_transform.origin, 
-		mouseMovement, 
+		mouse_movement, 
+		states,
 		velocity, 
 		handle_direction(), 
 		fps,
--- gorillabrawl/Scripts/UI/ui_menu.gd
+++ gorillabrawl/Scripts/UI/ui_menu.gd
@@ -19,11 +19,23 @@ func toggle_debug_label(visibility: bool):
 	for label in get_tree().get_nodes_in_group("debug_labels"):
 		label.visible = visibility
 
-func _on_player_debug_info(player_pos, mouse_pos, velocity, direction, fps, speed):
+func _on_player_debug_info(player_pos, mouse_pos, states, velocity, direction, fps, speed):
 	$VBoxContainer/CoordinatesLabel.text = "Position: %.2v" % player_pos
 	
 	$VBoxContainer/MouseCoordinatesLabel.text = "Mouse: %.0v" % mouse_pos
-
+	
+	var active_states = []
+	for state_name in states.keys():
+		if states[state_name]:
+			active_states.append(state_name)
+	
+	if active_states.size() == 0:
+		$VBoxContainer/StatesLabel.text = "States: None"
+	elif active_states.size() == 1:
+		$VBoxContainer/StatesLabel.text = "States: " + active_states[0]
+	else:
+		$VBoxContainer/StatesLabel.text = "States: " + ", ".join(active_states)
+	
 	$VBoxContainer/VelocityLabel.text = "Velocity: %.2v" % velocity
 	
 	$FacingDirectionLabel.text = "Direction: %s" % direction
